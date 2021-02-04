59b88dfb2a6196d7f260f2d9bc0bea3eee14cfa6
Bele <BelachewSh@ethiopianairlines.com>
2021-02-04 14:50:07 +0300

    --- Auto-Git Commit ---

--- lib/Views/SettingsPage.dart
+++ lib/Views/SettingsPage.dart
@@ -1,6 +1,4 @@
-import 'package:Flutter/Controller/CartController.dart';
 import 'package:Flutter/Controller/DialogController.dart';
-import 'package:Flutter/Controller/GetXDemoController.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,7 +10,7 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPage extends State<SettingsPage> {
-  final settingBuilder = Get.put(DialogController());
+  //final settingBuilder = Get.put(DialogController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,23 +21,20 @@ class _SettingsPage extends State<SettingsPage> {
         body: SafeArea(
           child: Column(
             children: [
-              Obx(
-                () {
-                  return Text("Count ${settingBuilder.count}");
-                  //Center(
-                  // child: Column(
-                  //mainAxisAlignment: MainAxisAlignment.center,
-                  // Text("Count ${settingBuilder.in}"),
-                  // RaisedButton.icon(
-                  //   onPressed: () {},
-                  //   label: Text("Click Me"),
-                  //   icon: new Icon(Icons.offline_bolt_sharp),
-                  // )
-                  //],
-                  //),
-                  // );
-                },
+              Text("Count"),
+              RaisedButton.icon(
+                onPressed: () {},
+                label: Text("Click Me"),
+                icon: new Icon(Icons.offline_bolt_sharp),
               ),
+              // Obx(() {
+              //   return Text(
+              //     'Total amount:',
+              //     style: TextStyle(fontSize: 32, color: Colors.black),
+              //   );
+              // }),
+              Obx(() => Text("obx")),
+              SizedBox(height: 25),
             ],
           ),
         ));
