5f5240e6e329b89da410afe4f8d2b6c4bac33e7f
Bele <BelachewSh@ethiopianairlines.com>
2021-02-05 11:56:18 +0300

    --- Auto-Git Commit ---

--- lib/Controller/SharedPref.dart
+++ lib/Controller/SharedPref.dart
@@ -1,16 +1,14 @@
-import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
-
 class SharedPref {
   read(String key) async {
     final prefs = await SharedPreferences.getInstance();
-    return json.decode(prefs.getString(key));
+    return prefs.getString(key);
   }
 
   save(String key, value) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setString(key, json.encode(value));
+    prefs.setString(key, List<>);
   }
 
   remove(String key) async {
--- lib/Services/API_Manager.dart
+++ lib/Services/API_Manager.dart
@@ -10,11 +10,12 @@ class Api_Manager {
     var newsModel = <MakeUpModel>[];
 
     try {
-      SharedPref sharedPref;
-      if (sharedPref?.read(keys.sharedPreferenses) != null) {
-        var jsonString = sharedPref?.read(keys.sharedPreferenses);
-        var jsonMap = json.decode(jsonString);
-        for (var items in jsonMap) {
+      SharedPref sharedPref = new SharedPref();
+      var sharedPrefResult = await sharedPref?.read(keys.sharedPreferenses);
+      if (sharedPrefResult != null) {
+        //var jsonString = sharedPref?.read(keys.sharedPreferenses);
+        //var jsonMap = json.decode(sharedPrefResult);
+        for (var items in sharedPrefResult) {
           final mekups = new MakeUpModel(
             imageLink: items['image_link'],
             id: items['id'],
@@ -30,9 +31,9 @@ class Api_Manager {
       } else {
         var response = await client.get(keys.url);
         if (response.statusCode == 200) {
-          sharedPref.save(keys.sharedPreferenses, response.body);
           var jsonString = response.body;
           var jsonMap = json.decode(jsonString);
+          await sharedPref.save(keys.sharedPreferenses, jsonMap);
           for (var items in jsonMap) {
             final mekups = new MakeUpModel(
               imageLink: items['image_link'],
