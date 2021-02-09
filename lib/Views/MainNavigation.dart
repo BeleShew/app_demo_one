e4bbe2d2004077535f678f36173032b8a8b92924
Bele <BelachewSh@ethiopianairlines.com>
2021-02-09 09:35:10 +0300

    --- Auto-Git Commit ---

--- lib/Views/MainNavigation.dart
+++ lib/Views/MainNavigation.dart
@@ -5,7 +5,7 @@ class MainNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
+      child: ListView(
         children: [
           Container(
             width: double.infinity,
@@ -37,6 +37,80 @@ class MainNavigation extends StatelessWidget {
                 ],
               ),
             ),
+          ),
+          ListTile(
+            leading: Icon(Icons.perm_identity),
+            title: Text(
+              'Profile',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.account_balance_outlined),
+            title: Text(
+              'Account Balance',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text(
+              'Settings',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.logout,
+            ),
+            title: Text(
+              'Logout',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.info_outline),
+            title: Text(
+              'About',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.room_rounded),
+            title: Text(
+              'Location',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.label_off),
+            title: Text(
+              'No updates',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.add_alert),
+            title: Text(
+              'Updates',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text(
+              'Version',
+              style: TextStyle(fontSize: 16),
+            ),
+            onTap: () {},
           ),
         ],
       ),
