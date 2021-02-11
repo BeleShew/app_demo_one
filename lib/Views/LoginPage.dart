98adbf2ef165470576178c0085aad977ddc076e5
Bele <BelachewSh@ethiopianairlines.com>
2021-02-11 11:50:55 +0300

    --- Auto-Git Commit ---

--- lib/Views/LoginPage.dart
+++ lib/Views/LoginPage.dart
@@ -20,11 +20,58 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-        centerTitle: true,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Login'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            new Stack(
+              alignment: Alignment.bottomCenter,
+              children: <Widget>[
+                //Image.asset('assets/collaboration.png', width: MediaQuery.of(context).size.width/1.5,),
+                WavyHeader(),
+                new Container(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  height: 192.0,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: new AssetImage(
+                        "assets/images/cat.jpg",
+                      ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: ShapeDecoration(
+                    shape: CircleBorder(),
+                    gradient: LinearGradient(
+                        colors: aquaGradients,
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight),
+                  ),
+                  child: ImageIcon(
+                    AssetImage("assets/images/cat.jpg"),
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                )
+                // Stack(
+                //   alignment: Alignment.bottomLeft,
+                //   children: <Widget>[
+                //     WavyFooter(),
+                //     CirclePink(),
+                //     CircleYellow(),
+                //   ],
+                // )
+              ],
+            ),
+          ],
+        ));
   }
 }
 
@@ -85,4 +132,80 @@ class TopWaveClipper extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class WavyFooter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: FooterWaveClipper(),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: aquaGradients,
+              begin: Alignment.center,
+              end: Alignment.bottomRight),
+        ),
+        height: MediaQuery.of(context).size.height / 3,
+      ),
+    );
+  }
+}
+
+class FooterWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(size.width, 0.0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0.0, size.height);
+    path.lineTo(0.0, size.height - 60);
+    var secondControlPoint = Offset(size.width - (size.width / 6), size.height);
+    var secondEndPoint = Offset(size.width, 0.0);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class YellowCircleClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return null;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) => false;
+}
+
+class CircleYellow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0.0, 210.0),
+      child: Material(
+        color: Colors.yellow,
+        child: Padding(padding: EdgeInsets.all(140)),
+        shape: CircleBorder(side: BorderSide(color: Colors.white, width: 15.0)),
+      ),
+    );
+  }
+}
+
+class CirclePink extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(-70.0, 90.0),
+      child: Material(
+        color: Colors.pink,
+        child: Padding(padding: EdgeInsets.all(120)),
+        shape: CircleBorder(side: BorderSide(color: Colors.white, width: 15.0)),
+      ),
+    );
+  }
 }
