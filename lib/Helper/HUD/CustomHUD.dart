8081ade50d9b6c1a882a97fcbda3687942366763
Bele <BelachewSh@ethiopianairlines.com>
2021-02-17 16:43:50 +0300

    --- Auto-Git Commit ---

--- lib/Helper/HUD/CustomHUD.dart
+++ lib/Helper/HUD/CustomHUD.dart
@@ -3,10 +3,24 @@ import 'dart:ffi';
 import 'package:bmprogresshud/progresshud.dart';
 import 'package:flutter/widgets.dart';
 
-class HudProvider extends Dismissible {
-  Void show(BuildContext context) {
-    ProgressHud.of(context).show(ProgressHudType.loading, "loading...");
+class HudProvider extends IDisposable {
+  
+  var hud = ProgressHud;
+  Void show() {
+    // ProgressHud.of(context).show(ProgressHudType.loading, "loading...");
+
+    //hud.show(ProgressHudType.progress, "loading");
   }
+
+  @override
+  void Dispose() {
+    //var hud = ProgressHud.of(context);
+   // hud.dismiss();
+  }
+}
+
+abstract class IDisposable {
+  void Dispose();
 }
 
 class IHudInterface {}
