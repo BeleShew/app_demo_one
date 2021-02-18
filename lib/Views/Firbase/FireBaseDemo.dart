ae3c4bb2bedc3f4a63b1481c71a2a8c91018e5d1
Bele <BelachewSh@ethiopianairlines.com>
2021-02-18 09:26:53 +0300

    --- Auto-Git Commit ---

---
+++ lib/Views/Firbase/FireBaseDemo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FireBaseApp extends StatefulWidget {
+  @override
+  _FireBaseAppState createState() => _FireBaseAppState();
+}
+
+class _FireBaseAppState extends State<FireBaseApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
