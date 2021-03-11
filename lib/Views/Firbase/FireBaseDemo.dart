f0a75293ee45034339f59ab56d58a66a13da9cc4
Bele <BelachewSh@ethiopianairlines.com>
2021-03-11 11:00:46 +0300

    --- Auto-Git Commit ---

--- lib/Views/Firbase/FireBaseDemo.dart
+++ lib/Views/Firbase/FireBaseDemo.dart
@@ -1,5 +1,4 @@
 import 'package:Flutter/Services/AuthServices.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class FireBaseDemoApp extends StatefulWidget {
